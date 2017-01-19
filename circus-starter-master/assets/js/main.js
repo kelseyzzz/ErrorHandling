e777e92762b1bad48338481a5f3bcc53b19905d6
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-01-19 17:20:57 -0500

    answering questions

--- circus-starter-master/assets/js/main.js
+++ circus-starter-master/assets/js/main.js
@@ -55,3 +55,75 @@ function showText() {
 }
 
 
+
+// ANSWERS
+
+
+// 1) Shuffle
+//    Code:
+
+ //   Link:
+
+
+// 2) Common Test
+
+//          Name: FizzBuzz
+
+//          Code: public class Test 
+//                {
+//                public static void main(String[] args)  
+//                {
+//                String whack =  "Whack", pop = "pop";
+//                for (int i = 1; i <= 100; i++)
+//                {
+//                if (i % 77 == 0) //check if number in position i is divisable by 15, if so don't check other 2 conditions - we don't want a double print
+//                {
+//                  System.out.println(whack + pop + " " + i));
+//                }
+//                else if (i % 7  == 0 )
+//                {
+//                  System.out.println(whack + " " + i);
+//                }
+//                else if (i % 11== 0)
+//                {
+//                  System.out.println(pop + " "+ i);
+//                }
+//                }
+//                }
+//                }
+
+//    Link: http://www.java67.com/2015/10/how-to-solve-fizzbuzz-in-java.html
+
+
+//3) Email Validation
+
+//          Code: 
+                //get the email address field content by id, here id=email
+                //var email =  $('#email').val();   
+                 
+                //this is the regular expression to match
+                //var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+                 
+                //then finally, you test the email using the pattern
+                 
+                //var validEmail = pattern.test(email);  
+                 
+                //the variable validEmail will only return true if the email matches the pattern 
+
+    //Link: https://www.quora.com/What-is-the-process-to-validate-an-email-address-in-JavaScript
+
+
+//4) Window Outer Width
+
+//          Info:
+
+//            Window.outerWidth
+
+//              Chrome: 1
+//              Firefox(Gecko): 1.0 ( 1.7 or earlier)
+//              Internet Explorer: 9
+//              Opera: 9
+//              Safari: 3
+
+//          Link: https://developer.mozilla.org/en-US/docs/Web/API/Window/outerWidth
+
