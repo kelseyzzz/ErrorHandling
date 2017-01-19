dc4dbaa6b61a3600ae3f26bf6f4a92147454779d
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-01-19 13:54:55 -0500

    fixed issues

--- main.js
+++ main.js
@@ -4,54 +4,45 @@ function createBubble() {
   var size = Math.round(Math.random() * 100 + 1);
   bubble.style.width = size + "px";
   bubble.style.height = size + "px";
-  bubble.style.top = (window.innerHeight - size) + "px";
+  bubble.style.top = (window.innerHeight - size) + px;
   bubble.style.left = (Math.round(Math.random() * (window.innerWidth + size) + 1)) + "px";
 
   document.body.appendChild(bubble);
 
-  var speed = Math.round(Math.random()*50+50);
-  var direction = Math.round(Math.random()*20 - 10);
+  var speed;
+  var dirction = Math.round(Math.random()*20 - 10);
 
-  var moveInterval = setInterval(function() {
-     var newTop = parseInt(bubble.style.top) - 10;
-     var newLeft = parseInt(bubble.style.left) + direction;
+  var moveInterval = setInterval(function(){
+    newTop = parseInt(bubble.style.top) - 10;
+    newLeft = parseInt(bubble.style.left) + direction;
     bubble.style.top = newTop + "px";
     bubble.style.left = newLeft + "px";
 
     if ( newTop < -100 ||
         newLeft < -100 ||
-        newLeft > window.innerWidth ) {
+        newLeft > window.innerWidth )
         window.clearInterval(moveInterval);
-
-      }
-
-    });
-
-  } 
-
-
-
-window.onload = function() {
-    var button = document.querySelector("#btn");
-
-    button.addEventListener("click", function() {
-      button.style.display = "none";
-      for ( var i = 0; i < 100; i++ ) {
-        createBubble();
     }
 
-      showText();
+    changeColor();
+  }, speed)
 
-  });
+}
+
+window.onload = functoin() {
+  button.addeventlistener("click", function() {
+    var button = document.querySelector("#btn");
+    button.style.display = "none";
+    for ( i = 0; i < 100; i++ ) {
+      createBubble;
+      console.log("This bubble's size is " + size + "and it's speed is " + speed + ".");
+    }
+  };
 }
 
 function showText() {
   var text = document.getElementsByTagName('p')[0];
-  var studentName = "Kelsey Zahand";
-  text.innerHTML += ", " + studentName + "!";
+  //Insert Student Name Here
+  text.innerHTML += ", ' + studentName + "!";
   text.style.opacity = 1;
-
-
 }
-
-
